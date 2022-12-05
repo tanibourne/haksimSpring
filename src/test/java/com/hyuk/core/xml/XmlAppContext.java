07e60d7a09a29ee7fde37b0680057e8f8eb9086f
tanibourne <tanibourne@gmail.com>
2022-12-05 21:55:14 +0900

    Spring Bean,container xml,config

---
+++ src/test/java/com/hyuk/core/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package com.hyuk.core.xml;
+
+import com.hyuk.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext applicationContext = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+
+    }
+}
